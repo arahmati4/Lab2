6c9f9c52018e6a7481aa4b589781cbaf4833fbfb
arahmati4 <arahmati4@myseneca.ca>
2024-02-12 21:34:26 +0000

    Added helloWorld function

--- lab2.py
+++ lab2.py
@@ -3,3 +3,8 @@ import sys
 def print_arguments():
     print("Script name:", sys.argv[0])
     print("Arguments passed:", sys.argv[1:])
+
+def helloWorld():
+    print('Hello World')
+
+helloWorld()
